e2d1948e524ae5e8c2ae3834d3fefca9d5178028
SYED ABDUL BARR ANAS <156329571+AnasSyed05@users.noreply.github.com>
2024-02-16 00:04:07 +0530

    factors_sqrt_n.cpp

--- factors_sqrt_n.cpp
+++ factors_sqrt_n.cpp
@@ -4,8 +4,24 @@ using namespace std;
 int main()
 {
 	int n;cin>>n;
-				//i*i<=n --> i<=√n
+		    //i*i<=n --> i<=√n
 	for(int i=1 ; i*i<=n ; i++){
-		if(n%i==0){cout<<i<<" "<<n/i<<endl;}			// i*n/i = n  i.e factor1 * factor2 = ognumber
+		if(n%i==0){
+		   if(i!=(n/i)){cout<<i<<" "<<n/i<<endl;}		// i*n/i = n  i.e factor1 * factor2 = ognumber
+		   else{cout<<i<<endl;}					// for perfect squares n/i will be equal to i therefore counting only once	
 	}
+ }
+	//to print number of factors of a given number we can do..
+	int count=0;
+	for(int i=1 ; i*i<=n ; i++){
+		if(n%i==0){
+		count++; 	//i is a factor
+
+	   	 if(i!=(n/i))    // for perfect squares n/i will be equal to i therefore counting only once 
+			count++;   //(n/i) is also a  factor
+		}
+	}
+	cout<<count<<endl;	//prints the number of factors of a given number
+
+	//we can check for prime num also if count == 2
 }
